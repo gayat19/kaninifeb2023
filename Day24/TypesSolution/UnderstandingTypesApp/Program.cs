853eb17c7f24571b17c968979a57d8e169650c32
Gayathri Mahadevan <gayat_19@yahoo.com>
2023-03-21 17:00:33 +0530

    Day24

---
+++ Day24/TypesSolution/UnderstandingTypesApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingTypesApp
+{
+    class Program
+    {
+        static int number1, number2;
+        static void TakeTwoNumbers()
+        {
+            Console.WriteLine("Please enter the first number");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number");
+            number2 = Int32.Parse(Console.ReadLine());
+        }
+        private static void Add()
+        {
+            int sum = number1 + number2;
+            Console.WriteLine("The sum of " + number1 + " and " + number2 + " is " + sum);
+            //Console.WriteLine("The sum of {0} and {1} is {2}", number1, number2, sum);
+        }
+        static void Main(string[] args)
+        {
+            TakeTwoNumbers();
+            Add();
+            Console.ReadKey();
+        }
+
+        
+    }
+}
